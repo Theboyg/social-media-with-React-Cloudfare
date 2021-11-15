c045b3ca2a45faedff3729a4f442884033ad22a6
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-11-14 18:57:44 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const body = await request.json();
+  return new Response(JSON.stringify(body));
+}
