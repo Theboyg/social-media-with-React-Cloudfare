44b5150cfbc6291011e3ed1fbe3223a331a4cabf
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-11-15 15:57:03 -0800

    setup dev environment/house cleaning react files/ upload asset/

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  const body = await request.json();
-  return new Response(JSON.stringify(body));
+
+  const CLIENT_ID =""
+
+  const resp = await fetch("", {
+
+    headers: {
+      
+      Authorization: `CLIENT_ID ${CLIENT_ID}`
+    }
+  })
+
+  const data = resp.json();
+  return new Response(JSON.stringify(data), {
+    
+    'content-type': 'application/json'
+  })
+  
+
+  const body = await request.json()
+  return new Response(JSON.stringify(body))
 }
+
+async function getData() {
+  
+  const cacheKey = `data`
+
+}
+
