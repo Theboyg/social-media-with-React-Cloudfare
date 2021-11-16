3cc1c63f2e83dfaed9fe2d2b6f8770d6e7f4097e
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-11-15 20:08:29 -0800

    homepage/topbar component is complete

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,54 @@
+import "./topbar.css";
+import { Search , Person, Chat, Notifications} from "@material-ui/icons";
+
+export default function Topbar() {
+    return (
+        <div className ="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo"> Gaoussou's  Social </span>
+            </div>
+
+            <div className="topbarCenter">
+
+                <div className="searchbar">
+
+                <Search className="searchIcon"/>
+                <input placeholder =" search for friend, post or video" className="searchInput" />
+                </div>
+            </div>
+           
+            <div className="topbarRight">
+                <div className="topbarLink">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconsItem">
+
+                        <Person></Person>
+                        <span className="topbarIconBadge">1</span>  
+
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconsItem">
+
+                        <Chat/>
+                        <span className="topbarIconBadge">2</span>
+
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconsItem">
+
+                        <Notifications/>
+                        <span className="topbarIconBadge">1</span>
+
+                    </div>
+                </div> 
+                <img src="/assets/assets/Person/1.jpg" alt="" className="topbarImage" />
+            </div>
+
+        </div>
+    )
+}
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,8 @@
+import Topbar from "../../components/topbar/Topbar";
+//import { Person } from "@material-ui/icons";
+
+export default function Home() {
+
+    return <Topbar />
+    
+}
