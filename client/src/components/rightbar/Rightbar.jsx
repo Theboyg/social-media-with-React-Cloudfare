54f8f3299dba57023540043d5315ed27511016dc
Gaoussou Diallo <gaoussou1993@gmail.com>
2021-11-21 17:56:03 -0800

    rightbar component is complete thus the homepage layout is complete. Next task is to give some functionality to the homepage

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,75 @@ import  "./rightbar.css"
 export default function  Rightbar() {
     return (
         <div className = "rightbar" >
-            rightbar
+           
+            <div className="rightbarWrapper">
+
+                <div className="birthdayContainer">
+
+                    <img src="assets/assets/gift.png" alt="" className="birthdayImg" />
+
+                    <span className="birthdayText">
+                       <b>Ellie</b>  and <b> 3 other friends </b> have birthday today! 
+                    </span>
+                </div>
+                <img src="assets/assets/ad.jpg" alt="" className="birthdayAd" />
+
+                <h4 className="rightbarTitle"> Online Friends </h4>
+
+                <ul className="rightbarFriendList">
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+
+                            <img src="assets/assets/person/6.jpg" alt="" className="rightbarProfileImg" />
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                         <span className="rightbarUsername"> Ashley Carter </span>
+                    </li>
+
+                     <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            
+                            <img src="assets/assets/person/6.jpg" alt="" className="rightbarProfileImg" />
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                         <span className="rightbarUsername"> Ashley Carter </span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            
+                            <img src="assets/assets/person/6.jpg" alt="" className="rightbarProfileImg" />
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                         <span className="rightbarUsername"> John Haminghton </span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            
+                            <img src="assets/assets/person/6.jpg" alt="" className="rightbarProfileImg" />
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                         <span className="rightbarUsername"> Gaoussou Diallo </span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            
+                            <img src="assets/assets/person/6.jpg" alt="" className="rightbarProfileImg" />
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                         <span className="rightbarUsername"> Djenebou Diakite </span>
+                    </li>
+ 
+                </ul>
+            </div>
         </div>
     )
 }
